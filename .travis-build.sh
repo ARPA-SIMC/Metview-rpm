afae91c301188da787da50575388da59411c46c9
Daniele Branchini <dbranchini@arpa.emr.it>
2019-09-23 10:11:29 +0200

    updated copr token, updated fedora builds

--- .travis-build.sh
+++ .travis-build.sh
@@ -7,7 +7,9 @@ if [[ $image =~ ^centos: ]]
 then
     pkgcmd="yum"
     builddep="yum-builddep"
+    builddepopt="--disablerepo=centos-sclo-rh"
     sed -i '/^tsflags=/d' /etc/yum.conf
+    yum update -q -y
     yum install -q -y epel-release
     yum install -q -y @buildsys-build
     yum install -q -y yum-utils
@@ -23,16 +25,19 @@ elif [[ $image =~ ^fedora: ]]
 then
     pkgcmd="dnf"
     builddep="dnf builddep"
+    builddepopt=""
     sed -i '/^tsflags=/d' /etc/dnf/dnf.conf
-    dnf install -q -y @buildsys-build
+    dnf update -q -y
+    dnf install --allowerasing -q -y @buildsys-build
     dnf install -q -y 'dnf-command(builddep)'
     dnf install -q -y git
     dnf install -q -y rpmdevtools
     dnf install -q -y pv
+    dnf install -q -y 'dnf-command(copr)'
     dnf copr enable -q -y simc/stable
 fi
 
-$builddep -q -y Metview.spec
+$builddep -q -y $builddepopt Metview.spec
 
 if [[ $image =~ ^fedora: || $image =~ ^centos: ]]
 then
